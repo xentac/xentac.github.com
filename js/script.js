15e726711cb3f666b2f37ad59de3e45865e1860b
Jason Chu <jchu@xentac.net>
2011-09-30 10:11:17 -0700

    Make footer nav tipsy messages show up on top

--- js/script.js
+++ js/script.js
@@ -4,5 +4,6 @@
 
 $(function() {
     $('nav ul li a').tipsy({fade: true, gravity: 'n'});
+    $('footer nav ul li a').tipsy({fade: true, gravity: 's'});
 	$('.jump-link').tipsy({gravity: 's',fade: true,fallback:'Read More'});
 })
