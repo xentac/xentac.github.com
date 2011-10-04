9c40c7d7f28e0a745d4212c1599c8b8dd9039bb1
Jason Chu <jchu@xentac.net>
2011-10-03 17:02:37 -0700

    Add post dates in tipsy popups for previous posts

--- js/script.js
+++ js/script.js
@@ -5,5 +5,6 @@
 $(function() {
     $('nav ul li a').tipsy({fade: true, gravity: 'n'});
     $('footer nav ul li a').tipsy({fade: true, gravity: 's'});
+    $('aside ul.posts li a').tipsy({fade: true, gravity: 'w'});
 	$('.jump-link').tipsy({gravity: 's',fade: true,fallback:'Read More'});
 })
